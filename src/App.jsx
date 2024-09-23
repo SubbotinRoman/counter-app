32addea219e951aea7f183a5aab29425d3c3947f
Roman Subbotin <roman.subbotin96@yandex.ru>
2024-09-23 20:38:18 +0300

    Верстка и стили проекта * сделал верстку и стили для проекта

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>0</h1>
+        <button className="minus">Минус</button>
+        <button className="plus">Плюс</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
